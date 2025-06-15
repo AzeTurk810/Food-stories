3b901f1b55883f56298589f6a190d69dded75d52
azeturk810 <azeturk810nf@gmail.com>
2025-06-15 15:16:48 +0400

    Remove all files

--- accounts/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-from django.contrib.auth.models import AbstractUser
-
-class User(AbstractUser):
-    image = models.ImageField(upload_to='images/',null=True, blank=True)
-    info = models.TextField()
-
-    
